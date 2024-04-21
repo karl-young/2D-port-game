15b33c24e759fc6c9aa6881170697434c30ffdb4
Karl Young <Karlyoung112@gmail.com>
2024-04-21 18:08:21 +1200

    Added left and right animation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { scaleFactor } from './constants.js'
-import { displayDialogue } from './utils'
+import { displayDialogue, setCamScale } from './utils'
 import { k } from './kaboomCtx.js'
 
 k.loadSprite('spritesheet', './spritesheet.png', {
@@ -78,16 +78,73 @@ k.scene('main', async () => {
     }
   }
 
+  setCamScale(k)
+
+  k.onResize(() => {
+    setCamScale(k)
+  })
+
   k.onUpdate(() => {
     k.camPos(player.pos.x, player.pos.y + 100)
   })
 
-
   k.onMouseDown((mouseBtn) => {
-    if (mouseBtn !== "left" || player.isInDialogue) return
+    if (mouseBtn !== 'left' || player.isInDialogue) return
 
     const worldMousePos = k.toWorld(k.mousePos())
     player.moveTo(worldMousePos, player.speed)
+
+    const mouseAngle = player.pos.angle(worldMousePos)
+
+    const lowerBound = 50
+    const upperBound = 125
+
+    if (
+      mouseAngle > lowerBound &&
+      mouseAngle < upperBound &&
+      player.curAnim() !== 'walk-up'
+    ) {
+      player.play('walk-up')
+      player.direction = 'up'
+      return
+    }
+
+    if (
+      mouseAngle < -lowerBound &&
+      mouseAngle > -upperBound &&
+      player.curAnim() !== 'walk-down'
+    ) {
+      player.play('walk-down')
+      player.direction = 'down'
+      return
+    }
+
+    if (Math.abs(mouseAngle) > upperBound) {
+      player.flipX = false
+      if (player.curAnim() !== 'walk-side') player.play('walk-side')
+      player.direction = 'right'
+      return
+    }
+
+    if (Math.abs(mouseAngle) < lowerBound) {
+      player.flipX = true
+      if (player.curAnim() !== 'walk-side') player.play('walk-side')
+      player.direction = 'left'
+      return
+    }
+  })
+
+  k.onMouseRelease(() => {
+    if (player.direction === 'down') {
+      player.play('idle-down')
+      return
+    }
+    if (player.direction === 'up') {
+      player.play('idle-up')
+      return
+    }
+
+    player.play('idle-side')
   })
 })
 
--- src/utils.js
+++ src/utils.js
@@ -21,9 +21,19 @@ export function displayDialogue(text, onDisplayEnd) {
   function onCloseBtnClick() {
     onDisplayEnd()
     dialogueUI.style.display = 'none'
-    dialogue.innerHTML = ""
+    dialogue.innerHTML = ''
     closeBtn.removeEventListener('click', onCloseBtnClick)
   }
-  
-  closeBtn.addEventListener("click", onCloseBtnClick)
+
+  closeBtn.addEventListener('click', onCloseBtnClick)
+}
+
+export function setCamScale(k) {
+  const resizeFactor = k.width() / k.height()
+  if (resizeFactor > 1) {
+    k.camScale(k.vec2(1))
+    return
+  }
+
+  k.camScale(k.vec2(1.5))
 }
