8a72a5ae029a3b78b0cabacc9bdc48cab16e6e90
Karl Young <Karlyoung112@gmail.com>
2024-04-21 16:43:35 +1200

    Added player

--- src/kaboomCtx.js
+++ src/kaboomCtx.js
@@ -1,7 +1,7 @@
-import kaboom from "kaboom";
+import kaboom from 'kaboom'
 
 export const k = kaboom({
   global: false,
   touchToMouse: true,
-  canvas: document.getElementById("#game"),
+  canvas: document.getElementById('game'),
 })
